51d00220b99ac82e1449fb59d4b1d7b95d3dce80
tomer shavit <tomer.shavit5@gmail.com>
2020-12-26 12:43:09 +0200

    changed the shop data to be an object and updated the reducers and the component to the new stored data

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionPreview } from "../../redux/shop/shop.selectors";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 import "./collections-overview.styles.scss";
 
@@ -14,7 +14,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/components/pages/collection-page/collection-page.component.jsx
+++ src/components/pages/collection-page/collection-page.component.jsx
@@ -4,9 +4,17 @@ import "./collection-page.styles.scss";
 import { connect } from "react-redux";
 import { selectCollection } from "../../../redux/shop/shop.selectors";
 
-const CollectionPage = ({ collection }) => {
-  console.log(collection);
-  return <div className="collection-page">Collection Page</div>;
+const CollectionPage = ({ collection: { title, items } }) => {
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item}></CollectionItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state, ownProps) => ({
